b27b457098fb1d8bad6b170a1532fd1b64dbbafe
mgc26 <matthew.g.crowson@gmail.com>
2023-03-08 11:47:23 -0500

    fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,14 +50,14 @@ raw_df = raw_df[raw_df['state'].isin(state_sub)]
 
 with tab1:
 
-    image = Image.open('title.jpg')
-    st.image(image, caption='Image is creative commons license')
+    image = Image.open('title.jpeg')
+    st.image(image, caption='COVID-19 virus image is creative commons license')
 
-    st.title("Welcome to our COVID-19 Healthcare Utilization Analysis App")
+    st.header("Welcome to our COVID-19 Healthcare Utilization Analysis App")
     st.markdown("In this repo, we have produced several dynamic figures using data pertaining to hospital utilization and the COVID-19 pandemic.")
     st.markdown("The COVID-19 pandemic has had a significant impact on healthcare utilization. While some patients have continued to seek emergency care for urgent medical needs, others have been hesitant to go to the health care institutions due to fears of contracting COVID-19. This has resulted in a change in healthcare utilization. The pandemic has also led to changes in the way care is delivered, including increased use of telemedicine and a greater focus on infection prevention and control measures. As the COVID-19 pandemic continues to evolve and in preparation for future public health crises, an analysis of healthcare utilization is prudent.")
     st.markdown("We will utilize two primary data sources to investigate the impact of the coronavirus pandemic on healthcare utilization in the US. Data on coronavirus cases and deaths will be obtained from the Centers for Disease Control and Prevention (CDC), which maintains geographic and temporal incidence data. Data on emergency department utilization and inpatient admissions will be derived from the Healthcare Cost and Utilization Project (HCUPnet): https://datatools.ahrq.gov/hcupnet. HCUP is a collection of healthcare databases developed through a public/private partnership and sponsored by the Agency for Healthcare Research and Quality (AHRQ). Data is collected by individual states and aggregated by AHRQ. Details regarding HCUP are available: https://www.hcup-us.ahrq.gov/") 
-    st.markdown("Brendin Beaulieu-Jones & Matthew Crowson")
+    st.markdown("Authors: Brendin Beaulieu-Jones & Matthew Crowson")
 
 with tab2:
     st.header("Plot 1")
@@ -209,5 +209,7 @@ with tab4:
 with tab5:
 
     st.header("Data")
+
+    st.markdown("In the spirit of reproducibility, we have included out curated database here.")
 
     st.dataframe(data=raw_df)
